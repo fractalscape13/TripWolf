7ca6d60fa295a68993c9d1ecd516ba257a06fd4e
Joseph Wangemann <fractalscape13@gmail.com>
2021-02-18 19:37:45 -0800

    remove unused screen

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from '@react-navigation/native';
 import LoadingScreen from '../screens/LoadingScreen';
 import Header from '../components/Header';
 import HomeScreen from '../screens/HomeScreen';
@@ -25,16 +24,6 @@ export const renderHomeStack = () => {
             headerStyle: headerStyle,
           }}
         />
-        {/* <HomeStack.Screen
-          name="EditProfileScreen"
-          component={EditProfileScreen}
-          options={{
-            headerTitle: () => <Header isHome={false} subheaderTitle={"Edit Profile"}/>,
-            headerStyle: headerStyle,
-            headerLeft: null,
-            headerRight: null
-          }}
-        /> */}
         <HomeStack.Screen
           name="LoadingScreen"
           component={LoadingScreen}
